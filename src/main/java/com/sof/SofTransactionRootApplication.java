0ba68a4d7dde2e70d58edcf5cd3d6ed52ab33129
Angelo-Ponce <angeloponcedev@gmail.com>
2025-03-05 16:11:11 -0500

    init project sofka

---
+++ src/main/java/com/sof/SofTransactionRootApplication.java
@@ -0,0 +1,13 @@
+package com.sof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofTransactionRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofTransactionRootApplication.class, args);
+    }
+
+}
