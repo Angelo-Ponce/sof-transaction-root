e889e147c5802ed4b301babfb05a022b381fb0d8
Angelo-Ponce <angeloponcedev@gmail.com>
2025-03-05 16:19:38 -0500

    add model Account and Movement

---
+++ src/main/java/com/sof/model/Account.java
@@ -0,0 +1,54 @@
+package com.sof.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="account")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "account_id")
+    private Long accountId;
+
+    @Column(name = "account_number", nullable = false, unique = true)
+    private Integer accountNumber;
+
+    @Column(name = "account_type", nullable = false, length = 100)
+    private String accountType;
+
+    @Column(name = "initial_balance", nullable = false, precision = 10, scale = 2)
+    private BigDecimal initialBalance;
+
+    @Column(nullable = false, length = 1)
+    private Boolean status;
+
+    @Column(name = "person_id", nullable = false)
+    private Long personId;
+
+    // Campos de auditoria
+    @Column(length = 30)
+    private String createdByUser;
+
+    @CreationTimestamp
+    @Column(name = "created_date", updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(length = 30)
+    private String lastModifiedByUser;
+
+    @UpdateTimestamp
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate;
+}
---
+++ src/main/java/com/sof/model/Movement.java
@@ -0,0 +1,64 @@
+package com.sof.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="movement")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Movement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long movementId;
+
+    @Column(name = "account_id", nullable = false)
+    private Long accountId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "movement_date", nullable = false)
+    private Date movementDate;
+
+    @Column(name = "movement_type", nullable = false, length = 150)
+    private String movementType;
+
+    @Column(name = "movement_value", nullable = false, precision = 10, scale = 2)
+    private BigDecimal movementValue;
+
+    @Column(name = "balance", nullable = false, precision = 10, scale = 2)
+    private BigDecimal balance;
+
+    @Column(nullable = false, length = 1)
+    private Boolean status;
+
+    @ManyToOne(fetch = FetchType.EAGER) // FK
+    @JoinColumn(name = "account_id", referencedColumnName = "account_id", foreignKey = @ForeignKey(name = "FK_MOVEMENT_ACCOUNT"), insertable = false, updatable = false)
+    private Account account;
+
+    // Campos de auditoria
+    @Column(length = 30)
+    private String createdByUser;
+
+    @CreationTimestamp
+    @Column(name = "created_date", updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(length = 30)
+    private String lastModifiedByUser;
+
+    @UpdateTimestamp
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate;
+}
